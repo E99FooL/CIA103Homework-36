ab0b7605832e523ed9c824fa7e4ff80b0f892652
Egg FooL <sandygina2324@yahoo.com.tw>
2024-08-22 16:38:56 +0800

    first commit

--- src/hw1/HW1_7.java
+++ src/hw1/HW1_7.java
@@ -1,15 +1,20 @@
-package hw1;
+
 /*請建立一個TestNineNine.java程式，可輸出九九乘法表
 • 一：使用for迴圈 + while迴圈
 • 二：使用for迴圈 + do while迴圈
 • 三：使用while迴圈 + do while迴圈*/
+package hw1;
+
 public class HW1_7 {
-	 public static void doubleForLoop() {
-	        for (int i = 1; i <= 9; i++) {
-	            for (int j = 1; j <= 9; j++) {
-	                System.out.print(i + " * " + j + " = " + (i * j) + "\t");
-	            }
-	            System.out.println();
-	        }
-	    }
-	}
+    public static void main(String[] args) {
+        // for 迴圈 + do while 迴圈
+        for (int i = 1; i <= 9; i++) {
+            int j = 1;
+            do {
+                System.out.printf("%d * %d = %d\t", i, j, i * j);
+                j++;
+            } while (j <= 9);
+            System.out.println();
+        }
+    }
+}
